ad34e3ccb2ef6aa2b820f5ab8c9890c3e4039600
softvision.agustin.ramos <agustin.ramos@softvision.com>
2021-12-21 09:45:55 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Conosur Tech
+          <a target="_blank" href="https://www.instagram.com/conosurtech/" rel="noreferrer">Ir a conosur tech</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
